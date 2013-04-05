2c83cd9bac08ecdb3952e513d25b97884c6920f9
robballou <rob.ballou@gmail.com>
2013-04-05 09:35:25 -0600

    Added some local commands to the mysql fabric libraries: local_dump and tables commands

--- fabfile/mysql.py
+++ fabfile/mysql.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from fabric.api import task, run
+from fabric.api import task, run, local
 import datetime
 
 @task
@@ -12,3 +12,21 @@ def dump(db, user='root'):
     """Backup database"""
     filename = "%s%s.sql" % (db, datetime.datetime.now().strftime("%Y%m%d%H%M"))
     run('mysqldump -u %(user)s -p %(db)s > %(filename)s' % {'user': user, 'db': db, 'filename': filename})
+
+@task
+def local_dump(db, user='root', password=True):
+    """Backup local database"""
+    filename = "%s%s.sql" % (db, datetime.datetime.now().strftime("%Y%m%d%H%M"))
+    cmd = "mysqldump -u %s " % user
+    if password == True:
+        cmd = "%s-p " % cmd
+    cmd = "%s%s > %s" % (cmd, db, filename)
+    local(cmd)
+
+@task
+def tables(database, user='root', password=True):
+    """Show a list of databases"""
+    cmd = "mysql -u %s " % user
+    if password == True:
+        cmd = "%s-p " % cmd
+    local('echo "show tables;" | %s %s' % (cmd, database))
