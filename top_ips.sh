3df43cd1da2cd3c497f61354b6e3066457da1484
robballou <rob.ballou@gmail.com>
2013-06-04 08:56:21 -0600

    Fix variables

--- top_ips.sh
+++ top_ips.sh
@@ -11,17 +11,19 @@ export DOMAIN=$1
 export USERNAME=$1
 
 if [[ -n "$2" ]]; then
-	export USERNAME=$2
+	USERNAME=$2
 fi
 
 export TLD="UNKNOWN"
 if [[ -f /home/$USERNAME/var/$DOMAIN.org/logs/transfer.log ]]; then
-	export TLD="org"
+	TLD="org"
 elif [[ -f /home/$USERNAME/var/$DOMAIN.com/logs/transfer.log ]]; then
-	export TLD="com"
+	TLD="com"
 elif [[ -f /home/$USERNAME/var/$DOMAIN.net/logs/transfer.log ]]; then
-	export TLD="net"
+	TLD="net"
 fi
+
+echo "/home/$USERNAME/var/$DOMAIN.$TLD/logs/transfer.log"
 
 if [[ $TLD -ne "UNKNOWN" ]]; then
 	awk '{ print $1}' /home/$USERNAME/var/$DOMAIN.$TLD/logs/transfer.log | sort  | uniq -c  | sort -nr | grep -v "^[[:space:]]*[[:digit:]][[:space:]]"
