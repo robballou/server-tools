34145506d01649ebe9e0d7ea735e659fb5846b15
robballou <rob.ballou@gmail.com>
2013-06-04 08:52:42 -0600

    Allow user and domain names are separate

--- top_ips.sh
+++ top_ips.sh
@@ -7,17 +7,24 @@ then
     exit 1
 fi
 
+export DOMAIN=$1
+export USERNAME=$1
+
+if [[ -n "$2" ]]; then
+	$USERNAME=$2
+fi
+
 export TLD="UNKNOWN"
-if [[ -f /home/$1/var/$1.org/logs/transfer.log ]]; then
+if [[ -f /home/$USERNAME/var/$DOMAIN.org/logs/transfer.log ]]; then
 	$TLD="org"
-elif [[ -f /home/$1/var/$1.com/logs/transfer.log ]]; then
+elif [[ -f /home/$USERNAME/var/$DOMAIN.com/logs/transfer.log ]]; then
 	$TLD="com"
-elif [[ -f /home/$1/var/$1.net/logs/transfer.log ]]; then
+elif [[ -f /home/$USERNAME/var/$DOMAIN.net/logs/transfer.log ]]; then
 	$TLD="net"
 fi
 
 if [[ $TLD -ne "UNKNOWN" ]]; then
-	awk '{ print $1}' /home/$1/var/$1.$TLD/logs/transfer.log | sort  | uniq -c  | sort -nr | grep -v "^[[:space:]]*[[:digit:]][[:space:]]"
+	awk '{ print $1}' /home/$USERNAME/var/$DOMAIN.$TLD/logs/transfer.log | sort  | uniq -c  | sort -nr | grep -v "^[[:space:]]*[[:digit:]][[:space:]]"
 else
 	echo "Could not find log"
 fi
