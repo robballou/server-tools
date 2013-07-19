29046429934e0625aed2fa66529518e173a2f734
robballou <rob.ballou@gmail.com>
2013-07-19 09:03:45 -0600

    Added some better handling for domains in the top_ip script

--- top_ips.sh
+++ top_ips.sh
@@ -7,6 +7,51 @@ then
     exit 1
 fi
 
+#
+# Find apachectl command
+#
+find_apache_config() {
+	apachectl=$(whereis apachectl)
+	if [[ $apachectl ]]; then
+		echo $apachectl
+	fi
+
+	apachectl=$(whereis apache2ctl)
+	if [[ $apachectl ]]; then
+		echo $apachectl
+	fi
+}
+
+#
+# Find the access log for the domain name
+#
+find_log_file() {
+	domain=$1
+
+	# find the apachectl command
+	apachectl=$(find_apache_config)
+	if [[ ! $apachectl ]]; then
+		echo 'Error: could not find apachectl/apache2ctl'
+		exit 1
+	fi
+
+	# find the vhost definition from the apachectl command
+	# "apachectl -S" returns a list of the configured vhosts
+	vhost=$((($apachectl -S) 2>&1) | grep $domain)
+
+	# now parse out the vhost configuration file
+	conf_file=$(echo $vhost | awk '{print $5}' | sed s/\(// | sed s/\)// | sed 's/:[[:digit:]]//')
+
+	# grep the configuration file for the access log
+	log=$(grep CustomLog $conf_file | awk '{print $2}' | sed 's/"//g')
+	if [[ -f $log ]]; then
+		echo $log
+	fi
+}
+
+
+#
+# Associate command line arguments with variable names
 export DOMAIN=$1
 export USERNAME=$1
 
@@ -14,19 +59,8 @@ if [[ -n "$2" ]]; then
 	USERNAME=$2
 fi
 
-export TLD="UNKNOWN"
-if [[ -f /home/$USERNAME/var/$DOMAIN.org/logs/transfer.log ]]; then
-	TLD="org"
-elif [[ -f /home/$USERNAME/var/$DOMAIN.com/logs/transfer.log ]]; then
-	TLD="com"
-elif [[ -f /home/$USERNAME/var/$DOMAIN.net/logs/transfer.log ]]; then
-	TLD="net"
-fi
-
-echo "/home/$USERNAME/var/$DOMAIN.$TLD/logs/transfer.log"
-
-if [[ $TLD != "UNKNOWN" ]]; then
-	awk '{ print $1}' /home/$USERNAME/var/$DOMAIN.$TLD/logs/transfer.log | sort  | uniq -c  | sort -nr | grep -v "^[[:space:]]*[[:digit:]][[:space:]]"
-else
-	echo "Could not find log"
+# get the domain log file
+log_file=$(find_log_file $DOMAIN)
+if [[ $log_file ]]; then
+	awk '{ print $1}' $log_file | sort  | uniq -c  | sort -nr | grep -v "^[[:space:]]*[[:digit:]][[:space:]]"
 fi
